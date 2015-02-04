57075b863e7b60b368d8bbce9171d49235acc2a7
Philip Washington Sorst <philip@sorst.net>
2015-02-04 11:42:01 +0100

    Test listNewsEntries, refs #2

--- src/Dontdrinkandroot/SymfonyAngularRestExample/RestBundle/Tests/Controller/NewsEntryControllerTest.php
+++ src/Dontdrinkandroot/SymfonyAngularRestExample/RestBundle/Tests/Controller/NewsEntryControllerTest.php
@@ -2,8 +2,39 @@
 
 namespace Dontdrinkandroot\SymfonyAngularRestExample\RestBundle\Tests\Controller;
 
+use Dontdrinkandroot\SymfonyAngularRestExample\BaseBundle\DataFixtures\ORM\NewsEntries;
+use Dontdrinkandroot\SymfonyAngularRestExample\BaseBundle\DataFixtures\ORM\Users;
+
 class NewsEntryControllerTest extends RestControllerTestCase
 {
+    public function testListNewsEntries()
+    {
+        $route = $this->getUrl('ddr_symfony_angular_rest_example_rest_newsentry_list_news_entries');
+
+        $this->client->request('GET', $route, [], [], ['HTTP_ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $content = $this->assertJsonResponse($response, 200);
+        $this->assertCount(2, $content);
+
+        $newsEntry2 = $this->getNewsEntryReference(NewsEntries::NEWS_ENTRY_2);
+        $user = $this->getUserReference(Users::ADMIN);
+        $expectedContent = [
+            'id'           => $newsEntry2->getId(),
+            'title'        => $newsEntry2->getTitle(),
+            'content'      => $newsEntry2->getContent(),
+            'date'         => $newsEntry2->getDate()->format('Y-m-d\TH:i:sO'),
+            'num_comments' => $newsEntry2->getNumComments(),
+            'author'       => [
+                'id'       => $user->getId(),
+                'username' => $user->getUsername()
+            ]
+        ];
+
+        $this->assertEquals($expectedContent, $content[1]);
+
+    }
+
     public function testGetNewsEntry()
     {
         $route = $this->getUrl('ddr_symfony_angular_rest_example_rest_newsentry_get_news_entry', ['id' => 1]);
@@ -13,7 +44,7 @@ class NewsEntryControllerTest extends RestControllerTestCase
 
         $content = $this->assertJsonResponse($response, 200);
 
-        $newsEntry = $this->getNewsEntryReference('news-entry-1');
+        $newsEntry = $this->getNewsEntryReference(NewsEntries::NEWS_ENTRY_1);
         $user = $this->getUserReference('user');
         $expectedContent = [
             'id'           => $newsEntry->getId(),
