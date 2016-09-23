38e270a9cb977f0b38623858f1df7057df2375c8
Philip Washington Sorst <philip@sorst.net>
2016-09-23 14:50:08 +0200

    Fixing tests

--- src/RestBundle/Controller/RestBaseController.php
+++ src/RestBundle/Controller/RestBaseController.php
@@ -17,14 +17,16 @@ class RestBaseController extends FOSRestController
         if ('json' === $request->getRequestFormat()) {
             /* We want a form with no name in the JSON REST API, as the content is not prefixed with the form name */
             $form = $this->container->get('form.factory')->createNamed('', $type, $data, $options);
-            /* Forms without a name are not submitted automatically if the data was empty. We want to enforce validation. */
+            $form->handleRequest($request);
+            /* Forms without a name are not submitted automatically if the data was empty.
+               We want to enforce validation. */
             if (!$form->isSubmitted()) {
                 $form->submit([]);
             }
         } else {
             $form = parent::createForm($type, $data, $options);
+            $form->handleRequest($request);
         }
-        $form->handleRequest($request);
 
         return $form;
     }
